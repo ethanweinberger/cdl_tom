cb54516193cae555e33dc16386bc91a068fb3103
ethan <ethan.weinberger@yale.edu>
2018-05-31 22:59:54 -0400

    removing old files

--- position.py
+++
@@ -1,8 +0,0 @@
-# Class for representing agent positions in an environment
-# Purpose of class is to make code more readable
-
-class Position(object):
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
