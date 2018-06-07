e4548c399edd0c16e64f7c6dfc4b24eb07208e81
ethan <ethan.weinberger@yale.edu>
2018-06-06 21:24:40 -0400

    Forward model test script

--- planning_example.py
+++ planning_example.py
@@ -1,59 +1,29 @@
-import csv
+"""
+Test script for forward model.
+"""
+
 from GridWorldMDP import GridWorldMDP
 from GridWorldMDP import make_grid_world_from_file
 from Planner import Planner
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--tau", type=float, default=0.005, help="Softmax parameter")
+parser.add_argument("--map", type=str, default="empty_map.mp", help="Map file name")
+args = parser.parse_args()
 
 def main():
-    # Setup MDP, Agents.
-    #mdp = GridWorldMDP(width=6, height=6, walls = [(1,2), (2,2), (3,2)],goal_locs=[(6, 6)])
     
-    mdp = make_grid_world_from_file("multi_goal.mp") 
-    value_iter = Planner(mdp, sample_rate=5)
-    value_iter.run_vi()
+    mdp = make_grid_world_from_file(args.map)
+    planner = Planner(mdp, sample_rate=5, tau=args.tau)
+    
+    planner.run_vi() 
 
-    # Visualize the map
     mdp.visualize_initial_map()
-
-    # Value Iteration.
-    action_seq, state_seq = value_iter.plan(mdp.get_init_state())
-
-    print("Plan for", mdp)
-    for i in range(len(action_seq)):
-        print("\t", action_seq[i], state_seq[i])
+    action_seq, state_seq, reward_seq = planner.plan(planner.mdp.get_init_state())
+    for state, action in zip(state_seq, action_seq):
+        print(state, action)
     
-    save_plan("test", "multi_goal", action_seq, state_seq)
-
-def save_plan(output_name, map_name, action_seq, state_seq):
-    """
-    Saves a plan (combination of action sequence and state sequence)
-    to a CSV file.  Our CSV is organized such that its columns are
-
-    Action    State.x    State.y
-
-    Args:
-        output_name (str): Name for our output CSV file
-        map_name (str): Name of the map that our planner navigated in
-        action_seq (str list): List of actions taken by our agent
-        state_seq (State list): List of states visited by our agent
-
-    Returns:
-        Nothing (outputs to CSV file)
-    """
-    data = []
-    column_names = ["Action", "State.x", "State.y", "Map_Name"]
-    data.append(column_names)
-
-    for action, state in zip(action_seq, state_seq):
-        new_row = [action, state.x, state.y, map_name]
-        data.append(new_row)
-
-    output_file  = open(output_name + ".csv", 'w')
-    with output_file:
-        writer = csv.writer(output_file)
-        writer.writerows(data)
-
-    
-
 if __name__ == "__main__":
     main()
-
